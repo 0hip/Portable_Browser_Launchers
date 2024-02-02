170695694b7435cdba937fdb718b3882bcabf8b1
0hip <90657954+0hip@users.noreply.github.com>
2024-02-01 23:28:27 -0500

    Add files via upload

--- THORIUM PORTABLE LAUNCHER.cmd
+++ THORIUM PORTABLE LAUNCHER.cmd
@@ -1,95 +1,153 @@
-@echo off
-title "THORIUM PORTABLE LAUNCHER"
-set "repo=/Alex313031/Thorium-Win"
-cd /d "%~dp0"
+@ECHO OFF
+SET "repo=/Alex313031/Thorium-Win"
+SET "user_dir=USER_DATA"
+SET "flags=--no-default-browser-check --disable-logging --disable-breakpad"
 
-if not exist bin\ (
-echo FIRST TIME RUNNING THORIUM PORTABLE LAUNCHER ...
-call :brandnew rel
-call :getweb
-call :download
-call :install
-call :run
+MODE CON: COLS=90 LINES=12 & COLOR 9F
+TITLE THORIUM PORTABLE LAUNCHER
+CD /D "%~dp0"
+
+SET test="%~dp0%user_dir%\Default\Cache\Cache_Data\index"
+IF EXIST %test% (
+   2>NUL (TYPE NUL>%test%) || (
+      ECHO THORIUM PORTABLE CURRENTLY RUNNING ?
+      GOTO norun
+   )
 )
-if exist new.zip (
-echo Applying the UPDATE ...
-goto resume
+IF NOT EXIST BIN\ (
+   ECHO FIRST TIME RUNNING THORIUM PORTABLE LAUNCHER ...
+   CALL :brandnew rel
+   CALL :getweb
+   CALL :download
+   CALL :unzip
+   CALL :run
 )
-if not exist "bin\thor_ver" (
-echo error: Current Version NOT A THORIUM RELEASE!
-echo Move THORIUM PORTABLE LAUNCHER to another folder and try again.
-pause
-exit
+IF EXIST new.zip (
+   ECHO Applying the UPDATE ...
+   GOTO resume
 )
-set "rel=none"
-set /P rel=<"bin\thor_ver"
-set "rel=%rel:*-=%"
-if %rel%==Win set "rel=AVX" 
-echo .. RELEASE TYPE: %rel%
-call :getlocal
-echo current version: %local%
-call :getweb
-echo _latest version: %tag%
-if %local%==%tag% (
-echo No new version found ...
-goto run
+IF NOT EXIST "BIN\thor_ver" (
+   ECHO error: Current Version NOT A THORIUM RELEASE!
+   ECHO Move THORIUM PORTABLE LAUNCHER to another folder and try again.
+   PAUSE
+   EXIT
 )
-echo NEW VERSION FOUND!
-call :download
+SET "rel=none"
+SET /P rel=<"BIN\thor_ver"
+SET "rel=%rel:*-=%"
+IF %rel%==Win SET "rel=AVX" 
+ECHO .. RELEASE TYPE: %rel%
+CALL :getlocal
+ECHO current version: %local%
+CALL :getweb
+ECHO _latest version: %tag%
+IF %local%==%tag% (
+   ECHO No new version found ...
+   GOTO run
+)
+ECHO NEW VERSION FOUND!
+CALL :download
 :resume
-::MOVING BIN TO _BIN as backup...
-2>nul (move /y bin _bin >nul) || (
-echo THORIUM PORTABLE IS CURRENTLY RUNNING [BIN folder is being used]
-echo Will apply the UPDATE on the next LAUNCH!
-ping -n 5 127.0.0.1>nul
-exit
+ECHO Backup BIN TO _BIN ...
+2>NUL (MOVE /Y BIN _BIN >NUL) || (
+   ECHO THORIUM PORTABLE IS CURRENTLY RUNNING [BIN folder is being used]
+   ECHO Will apply the UPDATE on the next LAUNCH!
+   PING -n 5 127.0.0.1>NUL
+   EXIT
 )
-call :install
+CALL :unzip
 :run
-if exist latest del latest
-set test="%~dp0USER_DATA\Default\Cache\Cache_Data\index"
-if exist %test% (2>nul (type nul>%test%) || goto norun)
-echo Starting THORIUM PORTABLE ...
-start "" "%~dp0bin\Thorium" --user-data-dir="%~dp0USER_DATA" --allow-outdated-plugins --disable-logging --disable-breakpad
+CALL :cleanup
+ECHO Starting THORIUM PORTABLE ...
+START "" "%~dp0BIN\Thorium" --user-data-dir="%~dp0%user_dir%" %flags%
 :norun
-ping -n 2 127.0.0.1>nul
-exit
+PING -n 3 127.0.0.1>NUL
+EXIT
+
 
 :getlocal
 set "local=0"
 for /f "delims=" %%G in (
-'dir /b /ad bin ^| findstr /r /v [a-Z] ^| findstr /r \.'
-) do set "local=%%G"
+   'dir /b /ad BIN ^| findstr /r /v [a-Z] ^| findstr /r \.'
+)do set "local=%%G"
 exit /b
 
 :getweb
-type nul>latest || (echo error: NO WRITE ACCESS & pause & exit)
+type nul>latest || (
+   echo error: NO WRITE ACCESS
+   pause
+   exit
+)
 CURL -s https://api.github.com/repos%repo%/releases/latest>latest
-::VERSION TAG
+:: VERSION TAG
 set "tag="
-for /f "delims=" %%G in ('type latest ^|find /i "tag_name"') do set "tag=%%G"
-if not defined tag (echo error: NOTHING FOUND! REPO CHANGED? & pause & exit)
+for /f "delims=" %%G in (
+   'type latest ^|find /i "tag_name"'
+)do set "tag=%%G"
+if not defined tag (
+   echo error: NOTHING FOUND! REPO CHANGED?
+   pause
+   exit
+)
 set "tag=%tag:*"M=%"
 set "tag=%tag:",=%"
-::DOWNLOAD URL
+:: DOWNLOAD URL
 set "url="
 for /f "delims=" %%G in (
-'type latest ^|find /i "browser_download_url" ^|find /i "_%rel%_" ^|find /i ".zip"'
-) do set "url=%%G"
-if not defined url (echo error: DOWNLOAD URL NOT FOUND. REPO CHANGED? & pause & exit)
+   'type latest ^|find "browser_download_url" ^|find "_%rel%_" ^|find ".zip"'
+)do set "url=%%G"
+if not defined url (
+   echo error: DOWNLOAD URL NOT FOUND. REPO CHANGED?
+   pause
+   exit
+)
 set "url=%url:*"https="https%"
+del latest
 exit /b
 
 :download
 echo Downloading latest THORIUM PORTABLE ...
-CURL -L -o new.zip %url% || (echo error: DOWNLOAD FAILED & pause & exit)
+CURL -L -o new.zip %url% || (
+   echo error: DOWNLOAD FAILED
+   pause
+   exit
+)
 cls
 exit /b
 
-:install
+:unzip
 echo Unzipping THORIUM ...
-tar -xf new.zip && del new.zip && if exist _bin/ rd /s /q _bin
+tar -xf new.zip || (
+   echo error: UNZIP failed. Corrupted ZIP file ? ... Cleaning up ...
+   del new.zip
+   if exist BIN/ rd /s /q BIN
+   if exist _BIN/ (
+      echo Restoring backup ...
+      move /y _BIN BIN
+      exit /b
+   )
+   exit
+)
+if exist _BIN/ rd /s /q _BIN
+del new.zip
 echo ... SUCCESS!
+exit /b
+
+:cleanup
+set "a=%user_dir%\Default"
+:: CACHE CLEANING
+REM call :cc "%~dp0%a%\Cache\"
+REM call :cc "%~dp0%a%\Code Cache\"
+:: FURTHER CLEANING
+REM call :cc "%~dp0%a%\IndexedDB\"
+REM call :cc "%~dp0%a%\Service Worker\"
+REM call :cc "%~dp0%a%\File System\"
+:: GET RID OF thorium_shell.exe
+REM set shell="%~dp0bin\%local%\thorium_shell.exe"
+REM if exist %shell% (del %shell% & echo deleted %shell%)
+exit /b
+:cc
+if exist %1 (rd /s /q %1 && echo removed %1)
 exit /b
 
 :brandnew
