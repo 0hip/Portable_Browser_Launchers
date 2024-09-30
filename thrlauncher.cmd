a76245b885747da8d04767804832baac33e21d13
0hip <90657954+0hip@users.noreply.github.com>
2024-09-29 19:51:03 -0500

    update thrlauncher.cmd

--- thrlauncher.cmd
+++ thrlauncher.cmd
@@ -2,7 +2,8 @@
 SET "repo=api.github.com/repos/Alex313031/Thorium-Win"
 SET "user_dir=USER_DATA"
 ::https://peter.sh/experiments/chromium-command-line-switches/
-SET "flags=--no-default-browser-check --disable-logging --disable-breakpad"
+::--disable-features=PrintCompositorLPAC [solves "print preview failed" issue]
+SET "flags=--no-default-browser-check --disable-logging --disable-breakpad --disable-features=PrintCompositorLPAC"
 
 MODE CON: COLS=90 LINES=12 & COLOR 9F
 TITLE THORIUM PORTABLE LAUNCHER
@@ -18,9 +19,7 @@ IF EXIST %test% (
 )
 IF NOT EXIST BIN\ (
    ECHO FIRST TIME RUNNING THORIUM PORTABLE LAUNCHER ...
-   CALL :brandnew rel
-   CALL :getweb
-   CALL :download
+   CALL :brandnew
    CALL :unzip
    CALL :getlocal
    CALL :run
@@ -40,7 +39,6 @@ FOR /f "skip=1" %%G IN (BIN\thor_ver) DO IF NOT DEFINED rel SET "rel=%%G"
 CALL :getweb
 IF NOT DEFINED tag GOTO run
 CALL :getlocal
-ECHO: CHECK FOR UPDATE ...
 ECHO: Release: %rel%
 ECHO: Current: %local%
 ECHO: Latest : %tag%
@@ -74,13 +72,14 @@ exit /b
 set "tag="
 set "url="
 ping -n 1 github.com>nul || exit /b
+echo: %rel% RELEASE : SEARCH FOR LATEST VERSION
 set "_dl=CURL --connect-timeout 7 -s https://%repo%/releases/latest"
 for /f "delims=" %%G in (
    '%_dl% ^|find "browser_download_url" ^|find ".zip" ^|find "_%rel%_"'
 )do set "url=%%G"
 if defined url (
    call :url_tag
-) else echo error: DOWNLOAD URL NOT FOUND. REPO CHANGED? 
+) else echo: %rel% RELEASE : NOT FOUND. REPO CHANGED? 
 exit /b
 :url_tag
 set "url=%url:*"https="https%"
@@ -134,15 +133,15 @@ exit /b
 
 :cleanup
 set "a=%user_dir%\Default"
+set shell="%~dp0bin\%local%\thorium_shell.exe"
 :: CACHE CLEANING
 rem call :cc "%~dp0%a%\Cache\"
 rem call :cc "%~dp0%a%\Code Cache\"
 :: FURTHER CLEANING
-rem call :cc "%~dp0%a%\IndexedDB\"
 rem call :cc "%~dp0%a%\Service Worker\"
 rem call :cc "%~dp0%a%\File System\"
+rem call :cc "%~dp0%a%\IndexedDB\"
 :: GET RID OF thorium_shell.exe
-rem set shell="%~dp0bin\%local%\thorium_shell.exe"
 rem if exist %shell% (del %shell% && echo deleted %shell%)
 exit /b
 :cc
@@ -156,14 +155,34 @@ setlocal enabledelayedexpansion
 choice /m "... THORIUM PORTABLE WILL BE DOWNLOADED AND LAUNCHED. CONTINUE?"
 if !ERRORLEVEL! EQU 2 exit
 cls
-set "a1=AVX2"
-set "a2=AVX"
-set "a3=SSE3"
-echo:########   RELEASE TYPES   ########
-echo: 1: %a1%    For most Intel/AMD CPUs since 2017.
-echo: 2: %a2%     For Intel/AMD CPUs since 2012.
-echo: 3: %a3%    For CPUs that lack AVX. Generally CPUs older than 2012.
+ping -n 1 github.com>nul || ( echo NO GITHUB RESPONSE & pause & exit )
+echo ##########   PORTABLE RELEASE(S)   ##########
+set "x=0"
+set "a="
+set "_dl=CURL --connect-timeout 7 -s https://%repo%/releases/latest"
+for /f "delims=" %%G in ('%_dl% ^|find "browser_download_url" ^|find ".zip"') do (
+	set /a "x+=1"
+	set "_u=%%G"
+	set "u!x!=!_u:*"https="https!"
+	set "_u=!_u:*/download/=!"
+	set "_u=!_u:*/=!"
+	set "_u=!_u:"=!"
+	echo: !x!: !_u!
+	set "a=!a!!x!"
+)
+set /a "x+=1"
+set "a=!a!!x!"
+echo: !x!: . . . I Have a URL (I know what I'm doing)
 echo:
-choice /C 123 /N /M ".. CHOOSE ONE [1,2,3]: "
-set "n=!a%ERRORLEVEL%!"
-endlocal& set %~1=%n%
+choice /C !a! /N /M ".. CHOOSE ONE: "
+set "_x=%ERRORLEVEL%"
+if !_x! EQU !x! (
+	set /p "u!x!=Paste URL here: " || exit /b
+	set "_u=!u%x%!"
+	set "_u=!_u:"=!"
+	if not "!_u:~-4!"==".zip" ( echo Invalid URL & pause & exit )
+)
+echo Downloading: !u%_x%!
+CURL -L -o new.zip !u%_x%!
+endlocal
+cls
